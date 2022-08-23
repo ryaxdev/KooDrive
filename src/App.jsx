7e381bc43ecdb573112ee32f11c07d7868376094
Leonardo <93416060+ryaxdev@users.noreply.github.com>
2022-08-23 18:55:32 +0000

    user

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
         {docus.map((doc) => (
           <li>
             <article>
-              <h3>username: {doc.nombre}</h3>
+              <h3>{user.name}</h3>
               <p>{doc.nombre}</p>
               <img src={doc.url} />
             </article>
